d3fa5158713b337a53ccb7a77260b14acf095e6e
Tim <tim.lisiecki@gmail.com>
2019-03-20 14:34:15 -0400

    Updates

---
+++ precache-manifest.5b6e93d2624171b2ad71f2d949d78947.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a79903681b862e539623",
+    "url": "/personal-website/static/css/main.b28acadc.chunk.css"
+  },
+  {
+    "revision": "a79903681b862e539623",
+    "url": "/personal-website/static/js/main.614d5a90.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "512afdd977ca8e1c23aa",
+    "url": "/personal-website/static/js/2.2ea67d1b.chunk.js"
+  },
+  {
+    "revision": "7785f787877589cfe2a7d75cea4671e3",
+    "url": "/personal-website/static/media/bcc.7785f787.png"
+  },
+  {
+    "revision": "519624a6537c491576b1b572972ded13",
+    "url": "/personal-website/static/media/webdev-bg.519624a6.jpg"
+  },
+  {
+    "revision": "24545c75aed605b7de9870366e695f13",
+    "url": "/personal-website/static/media/timlisiecki.24545c75.jpg"
+  },
+  {
+    "revision": "363f2fd90c5dac033245c4946ad3e8cc",
+    "url": "/personal-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/timlisiecki/personal-website/precache-manifest.f58f8b1ad21089f0172015d1acd50369.js"
+  "/personal-website/precache-manifest.5b6e93d2624171b2ad71f2d949d78947.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/timlisiecki/personal-website/index.html", {
+workbox.routing.registerNavigationRoute("/personal-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
