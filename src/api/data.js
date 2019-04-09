97a896ebb81a892958e0cc11f4e152deabd8333c
Tim <tim.lisiecki@gmail.com>
2019-04-09 16:00:35 -0400

    created an api directory for the data

---
+++ src/api/data.js
@@ -0,0 +1,363 @@
+export const data = {
+	projects: [
+		{
+			name: "Blue Collar Coding",
+			desc: "Founded a freelance business for building WordPress websites for small businesses.",
+			url: "https://bluecollarcoding.com",
+			image: "https://timlisiecki.github.io/images/portfolio/bcc.png",
+			featured: true,
+			tech: ["HTML", "CSS", "Wordpress"]
+		},
+		{
+			name: "Scratch Ticket App",
+			desc: "Scratch ticket game created using Javascript.",
+			url: "https://timlisiecki.github.io/web-dev-scratch-tickets/",
+			github: "https://github.com/timlisiecki/web-dev-scratch-tickets",
+			image: "https://timlisiecki.github.io/images/portfolio/scratch-tix.png",
+			featured: true,
+			tech: ["JavaScript" ,"HTML", "CSS"]
+		},
+		{
+			name: "RoboFriends App",
+			desc: "Progressive Web App using React and Redux.",
+			url: "https://timlisiecki.github.io/robofriends-with-redux/",
+			github: "https://github.com/timlisiecki/robofriends-with-redux",
+			image: "https://timlisiecki.github.io/images/portfolio/robofriends-redux.png",
+			featured: true,
+			tech: ["React", "Redux", "PWA"]
+		},
+		{
+			name: "React To Do App",
+			desc: "To Do app built with React. Uses third-party authorization to register/login.",
+			url: "http://reacttodoapp.herokuapp.com/",
+			github: "https://github.com/timlisiecki/city-ofchampions",
+			image: "https://timlisiecki.github.io/images/portfolio/react-todo-app.png",
+			featured: false,
+			tech: ["React", "HTML", "SCSS"]
+		},
+		{
+			name: "React Weather App",
+			desc: "Weather app built with React. Uses third-party authorization to register/login.  ",
+			url: "http://powerful-cliffs-34356.herokuapp.com/",
+			github: "https://github.com/timlisiecki/react-weather-app",
+			image: "https://timlisiecki.github.io/images/portfolio/react-weather-app.png",
+			featured: false,
+			tech: ["React", "HTML", "SCSS"]
+		},
+		{
+			name: "Budget App",
+			desc: "Object Oriented Programming JavaScript Budget App.",
+			url: "https://timlisiecki.github.io/budget-app/",
+			github: "https://github.com/timlisiecki/budget-app",
+			image: "https://timlisiecki.github.io/images/portfolio/budget.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS"]
+		},
+		{
+			name: "Yelp Camp",
+			desc: "Fictional web application for reviewing campsites, using Node.js, ExpressJS, Mongoose, Passport, RESTful Routing, jQuery, Javascript, and Bootstrap.",
+			url: "https://yelpcamp-demo.herokuapp.com/",
+			github: "https://github.com/timlisiecki/udemy-bootcamp-YelpCamp",
+			image: "https://timlisiecki.github.io/images/portfolio/yelpcamp.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS", "Express"]
+		},
+		{
+			name: "Recipe Website",
+			desc: "*Work in Progress* Fictional web application for recipes, using Node.js, ExpressJS, Mongoose, Passport, RESTful Routing, jQuery, Javascript, and Bootstrap.",
+			url: "https://recipe-website.herokuapp.com/recipes",
+			github: "https://github.com/timlisiecki/budget-app",
+			image: "https://timlisiecki.github.io/images/portfolio/recipewebsite.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS", "Express"]
+		},
+		{
+			name: "Dice Game",
+			desc: "2-Player dice game built by JavaScript.",
+			url: "https://timlisiecki.github.io/dice-game/",
+			github: "https://github.com/timlisiecki/dice-game",
+			image: "https://timlisiecki.github.io/images/portfolio/dice.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS"]
+		},
+		{
+			name: "Color Guessing Game",
+			desc: "Guessing game using JavaScript to choose the correct color square given a amount of red, green, blue (RGB) color system.",
+			url: "https://timlisiecki.github.io/guess_color/",
+			github: "https://github.com/timlisiecki/guess_color",
+			image: "https://timlisiecki.github.io/images/portfolio/guesscolor.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS"]
+		},
+		{
+			name: "City of Champions",
+			desc: "Object Oriented Programming JavaScript project displaying content dynamically.",
+			url: "https://timlisiecki.github.io/city-ofchampions/",
+			github: "https://github.com/timlisiecki/city-ofchampions",
+			image: "https://timlisiecki.github.io/images/portfolio/champions.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS"]
+		},
+		{
+			name: "Patatap Clone",
+			desc: "A clone of Patatap.com, utilizing JavaScript to animate the HTML5 Canvas to create colors and sounds by the keyboard. (Desktop-Only)",
+			url: "https://timlisiecki.github.io/patatap-clone/",
+			github: "https://github.com/timlisiecki/patatap-clone",
+			image: "https://timlisiecki.github.io/images/portfolio/patatapclone.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS"]
+		},
+		{
+			name: "Todo List",
+			desc: "Simple to do list using jQuery to add, complete, and delete a list item.",
+			url: "https://timlisiecki.github.io/to-do-list/",
+			github: "https://github.com/timlisiecki/to-do-list",
+			image: "https://timlisiecki.github.io/images/portfolio/todolist.png",
+			featured: false,
+			tech: ["JavaScript", "HTML", "CSS"]
+		},
+		{
+			name: "Resume ",
+			desc: "Unofficial resume application using JSON to fill in HTML template.",
+			url: "https://timlisiecki.github.io/udacity-frontend-resume/",
+			github: "https://github.com/timlisiecki/udacity-frontend-resume",
+			image: "https://timlisiecki.github.io/images/portfolio/resume.png",
+			featured: false,
+			tech: ["jQuery", "HTML", "CSS"]
+		}
+	],
+	jobs: [
+		{
+			company: "Blue Collar Coding",
+			title: "Web Developer",
+			dates: "Sep 2018 - Present",
+			desc: "Founder of Blue Collar Coding, a freelance website for developing WordPress websites for small businesses. Designing and developing websites using WordPress with user experience and interface in mind. Maintaining and updating websites as required. Currently part-time serving a limited number of clients.",
+			url: "https://bluecollarcoding.com",
+			industry: "technology"
+		},
+		{
+			company: "Elromco, Inc",
+			title: "Web Developer",
+			dates: "Dec 2018 - Mar 2019",
+			desc: "Web developer in a small team for a moving software startup. Main responsibilities include building custom websites for moving companies, integrating the software with clients’ websites, and creating processes to make company and products scalable. Also set up foundation for offering SEO services.",
+			url: "https://elromco.com",
+			industry: "technology"
+		},
+		{
+			company: "Massachusetts Bay Lines",
+			title: "Web Marketing Manager",
+			dates: "May 2017 - Dec 2018",
+			desc: "Web Marketing Manager with responsibilities in marketing and website management. Other responsibilities include training employees, creating and implementing 40-page training program, documenting processes and procedures, hands-on training, and assisting in scheduling and hiring.",
+			url: "https://massbaylines.com",
+			industry: "technology"
+		},
+		{
+			company: "Freelance",
+			title: "Front End Web Developer",
+			dates: "Jan 2016 - Dec 2018",
+			desc: "[part-time] Web Developer with a focus on Front End Development. Developing responsive design projects. Proficiency in HTML, CSS, and JavaScript. Utilizing frameworks and libraries such as Bootstrap, jQuery, React, and Node.",
+			url: "",
+			industry: "technology"
+		},
+		{
+			company: "Massachusetts Bay Lines",
+			title: "Captain",
+			dates: "Apr 2009 - Present",
+			desc: "[part-time/seasonal] Captain of fleet of five vessels in Boston Harbor. See LinkedIn profile for more information.",
+			url: "https://massbaylines.com",
+			industry: "maritime"
+		}
+	],
+	skills: [
+		{
+			name: "HTML",
+			type: "language-library",
+			level: "expert",
+			featured: true
+		},
+		{
+			name: "CSS",
+			type: "language-library",
+			level: "expert",
+			featured: true
+		},
+		{
+			name: "JavaScript",
+			type: "language-library",
+			level: "advanced",
+			featured: true
+		},
+		{
+			name: "Git",
+			type: "tools-technology",
+			level: "expert",
+			featured: false
+		},
+		{
+			name: "Responsive Web Design",
+			type: "industry-knowledge",
+			level: "expert",
+			featured: false
+		},
+		{
+			name: "Integrated Marketing",
+			type: "industry-knowledge",
+			level: "advanced",
+			featured: false
+		},
+		{
+			name: "SEO",
+			type: "industry-knowledge",
+			level: "advanced",
+			featured: false
+		},
+		{
+			name: "React",
+			type: "language-library",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "Redux",
+			type: "language-library",
+			level: "beginner",
+			featured: false
+		},
+		{
+			name: "Bootstrap",
+			type: "language-library",
+			level: "advanced",
+			featured: false
+		},
+		{
+			name: "jQuery",
+			type: "language-library",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "Node.js",
+			type: "tools-technology",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "Gulp",
+			type: "tools-technology",
+			level: "advanced",
+			featured: false
+		},
+		{
+			name: "SASS",
+			type: "language-library",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "Webpack",
+			type: "tools-technology",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "PHP",
+			type: "language-library",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "WordPress",
+			type: "tools-technology",
+			level: "advanced",
+			featured: false
+		},
+		{
+			name: "Google Analytics",
+			type: "tools-technology",
+			level: "intermediate",
+			featured: false
+		},
+		{
+			name: "Angular",
+			type: "language-library",
+			level: "beginner",
+			featured: false
+		}
+
+	],
+	education: [
+		{
+			school: "University of Massachusetts",
+			degree: "B.S. Management/Marketing",
+			location: "Boston, MA",
+			date: "May 2015",
+			image: "images/bcc.png"
+		}
+	],
+	resources: [
+		{
+			title: "The Complete Junior to Senior Web Developer Roadmap (2019)",
+			author: "Andrei Neagoie",
+			url: "https://www.udemy.com/the-complete-junior-to-senior-web-developer-roadmap/",
+			image: "https://i.udemycdn.com/course/240x135/1650610_2673_5.jpg",
+			type: "Course",
+			featured: true
+		},
+		{
+			title: "Automate Web Development with Gulp JS",
+			author: "Andrew Mead",
+			url: "https://www.udemy.com/learn-gulp/",
+			image: "https://i.udemycdn.com/course/240x135/284076_749f_7.jpg",
+			type: "Course",
+			featured: false
+		},
+		{
+			title: "The Complete React Web App Developer Course",
+			author: "Andrew Mead",
+			url: "https://www.udemy.com/the-complete-react-web-app-developer-course/",
+			image: "https://i.udemycdn.com/course/240x135/755128_8928.jpg",
+			type: "Course",
+			featured: false
+		},
+		{
+			title: "Git a Web Developer Job: Mastering the Modern Workflow",
+			author: "Brad Schiff",
+			url: "https://www.udemy.com/git-a-web-developer-job-mastering-the-modern-workflow/",
+			image: "https://i.udemycdn.com/course/240x135/818990_57c0_3.jpg",
+			type: "Course",
+			featured: false
+		},
+		{
+			title: "The Complete JavaScript Course: Build Real Projects!",
+			author: "Jonas Schmedtmann",
+			url: "https://www.udemy.com/the-complete-javascript-course/",
+			image: "https://i.udemycdn.com/course/240x135/851712_fc61_5.jpg",
+			type: "Course",
+			featured: false
+		},
+		{
+			title: "The Web Developer Bootcamp",
+			author: "Colt Steele",
+			url: "https://www.udemy.com/the-web-developer-bootcamp/",
+			image: "https://i.udemycdn.com/course/240x135/625204_436a_2.jpg",
+			type: "Course",
+			featured: false
+		},
+		{
+			title: "JavaScript - The Good Parts",
+			author: "Douglas Crockford",
+			url: "http://shop.oreilly.com/product/9780596517748.do",
+			image: "https://covers.oreillystatic.com/images/9780596517748/lrg.jpg",
+			type: "Book",
+			featured: true
+		},
+		{
+			title: "Programming JavaScipt Applications",
+			author: "Eric Elliott",
+			url: "https://www.oreilly.com/library/view/programming-javascript-applications/9781491950289/",
+			image: "https://www.oreilly.com/library/cover/9781491950289/360h/",
+			type: "Book",
+			featured: true
+		}
+	]
+};
